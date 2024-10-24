22c19445d2c90714f084b67e3feee270ac9375df
Josafat11 <116617414+Josafat11@users.noreply.github.com>
2024-10-24 10:46:53 -0600

    retoques3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
+import serverless from 'serverless-http';
 import app from './app.js'; // Importación de app.js desde src/
 import './database.js';      // Importación de database.js desde src/
 
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+// Exportar la app utilizando serverless-http para que Vercel lo maneje correctamente
+export const handler = serverless(app);
